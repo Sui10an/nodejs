04825ebf85efd59119cab212c4a52c1433a5534e
Sui10an <go314513an@gmail.com>
2024-09-13 16:25:01 +0900

    tabun

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
-	import { onMount, onDestroy } from 'svelte';
+	import { onMount } from 'svelte';
 	import { writable } from 'svelte/store';
 
 	const disp = writable(0);
+
+	let isStart = false;
 
 	onMount(() => {
 		let eventSource: EventSource;
@@ -38,6 +40,115 @@
 			}
 		};
 	});
+
+	let reset = 0;
+
+	const handleKeydown = (e: KeyboardEvent) => {
+		let timer;
+
+		if (e.key === 'Enter') {
+			e.preventDefault();
+			reset++;
+			clearTimeout(timer);
+
+			console.log(reset);
+
+			if (reset === 5) {
+				fetch('/api/countup', {
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json'
+					},
+					body: JSON.stringify({
+						value: '-'
+					})
+				})
+					.then((responce) => responce.json())
+					.then((data) => {
+						console.log('Success:', data);
+					})
+					.catch((e) => {
+						console.error('Error:', e);
+					});
+				reset = 0;
+				console.log('reset');
+			}
+
+			timer = setTimeout(() => {
+				reset = 0;
+			}, 2000);
+		}
+	};
+
+	let storeCount = 0;
+
+	$: console.log(storeCount)
 </script>
 
-<p>Count: {$disp}</p>
+<svelte:window on:keydown={(e) => handleKeydown(e)} />
+
+<div
+	class="w-screen h-screen text-sky-500 font-bold flex flex-col gap-10 items-center justify-center"
+>
+	{#if isStart}
+		<p id="count" class="text-xl">Count: {$disp}</p>
+		<button
+			class="p-4 border border-sky-400 rounded-lg shadow-lg shadow-sky-400/20"
+			on:click={() => {
+				isStart = false;
+				storeCount = Number(document.querySelector('#count')?.innerHTML.split(': ')[1]) | 0
+				console.log(storeCount)
+			}}
+		>
+			計測停止
+		</button>
+	{:else}
+		<p class="text-xl">計測停止中 store: {storeCount}</p>
+		<div class="flex gap-3">
+			<button
+				class="p-4 border border-sky-400 rounded-lg shadow-lg shadow-sky-400/20"
+				on:click={() => {
+					fetch('/api/countup', {
+						method: 'POST',
+						headers: {
+							'Content-Type': 'application/json'
+						},
+						body: JSON.stringify({
+							value: '-'
+						})
+					})
+						.then((responce) => responce.json())
+						.then((data) => {
+							console.log('Success:', data);
+						})
+						.catch((e) => {
+							console.error('Error:', e);
+						})
+					isStart = true;
+				}}
+			>
+				0から計測開始
+			</button>
+			{#if storeCount != 0}
+				<button
+					class="p-4 border border-sky-400 rounded-lg shadow-lg shadow-sky-400/20"
+					on:click={() => {
+						isStart = true;
+					}}
+				>
+					再開
+				</button>
+			{/if}
+		</div>
+		<div>
+			<button
+				class="p-4 border border-sky-400 rounded-lg shadow-lg shadow-sky-400/20"
+				on:click={() => {
+					window.location.href = `/result?co=${storeCount}`
+				}}
+			>
+				リザルト画面に移る
+			</button>
+		</div>
+	{/if}
+</div>
--- src/routes/api/countup/+server.ts
+++ src/routes/api/countup/+server.ts
@@ -3,35 +3,47 @@ import type { RequestHandler } from '@sveltejs/kit';
 import { count } from '$lib';
 
 export const GET: RequestHandler = async () => {
-    let streameClosed = false
+    let isClosed = false
 
     const stream = new ReadableStream({
         start(controller) {
             const send = (value: number) => {
-                if (!streameClosed) {
-                    const message = `data: ${JSON.stringify({ count: value })}\n\n`;
-                    try {
-                        controller.enqueue(message);
-                    } catch (err) {
-                        console.error("Error enqueuing message:", err);
-                    }
-                } else {
-                    console.log("stream is already closed")
+                if (isClosed) {
+                    console.log("Stream is already closed, skipping message")
+                    return
                 }
-            };
+                const message = `data: ${JSON.stringify({ count: value })}\n\n`;
+                try {
+                    controller.enqueue(message);
+                } catch (err) {
+                    console.error("Error enqueuing message:", err);
+                }
+            }
 
             const unsubscribe = count.subscribe(value => {
-                send(value);
-            });
+                send(value)
+            })
 
             const keepAliveInterval = setInterval(() => {
-                controller.enqueue(': keep-alive\n\n')
+                if (isClosed) {
+                    clearInterval(keepAliveInterval);
+                } else {
+                    try {
+                        controller.enqueue(': keep-alive\n\n');
+                    } catch (err) {
+                        clearInterval(keepAliveInterval);
+                        unsubscribe();
+                        controller.close();
+                        console.error("Stream closed, stopping keep-alive:", err);
+                    }
+                }
             }, 15000)
 
             controller.close = () => {
                 unsubscribe()
                 clearInterval(keepAliveInterval)
-            };
+                isClosed = true
+            }
         }
     })
 
@@ -43,18 +55,26 @@ export const GET: RequestHandler = async () => {
             'Access-Control-Allow-Origin': '*'
         }
     })
-};
+}
 
 //
 export const POST: RequestHandler = async ({ request }) => {
     const data = await request.json();
     console.log('[/api/countup] Received data:', data);
+    
+    let message = ''
 
     if (data.value === '*') {
-        count.update(n => n + 1);
+        count.update(n => n + 1)
+        message = '*'
     }
 
-    return new Response(JSON.stringify({ status: 200, message: '*' }), {
+    if (data.value === "-") {
+        count.update(() => 0)
+        message = '-'
+    }
+
+    return new Response(JSON.stringify({ status: 200, message: message }), {
         headers: { 'Content-Type': 'application/json' }
-    });
-};
+    })
+}
