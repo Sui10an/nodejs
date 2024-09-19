68075ef0c9c6e9ce96f96a30509eb9feedab6274
shiki <kishintorii@gmail.com>
2024-09-19 09:20:22 +0900

    save

--- stat.bat
+++ stat.bat
@@ -5,11 +5,7 @@ taskkill /F /IM chrome.exe
 REM すべてのNode.jsサーバーを停止する
 taskkill /F /IM node.exe
 
+start chrome.exe --start-fullscreen https://localhost:5173
+
 REM サーバーを起動
 cmd /k "npm run start" &
-
-REM 少し待機してからブラウザを開く
-timeout /t 4 /nobreak >nul
-
-REM Chromeを新しいウィンドウで開く
-start chrome --new-window http://localhost:5173
